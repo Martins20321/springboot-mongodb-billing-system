0d0fb82c700bd61d54c468f0bbc640199f3ea50d
Martins20321 <jgmsilva11@gmail.com>
2025-12-22 10:49:25 -0300

    Project created

---
+++ src/main/java/com/estudospringmartins/mongobilling/MongobillingApplication.java
@@ -0,0 +1,13 @@
+package com.estudospringmartins.mongobilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongobillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongobillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudospringmartins/mongobilling/MongobillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudospringmartins.mongobilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongobillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
